3d8d15f2045a32dd6aa8d665329bab1f5105488f
Ross Bulat <ross@jkrb.io>
2026-07-10 12:19:27 +0700

    chore: add known pool

--- packages/consts/src/pools.ts
+++ packages/consts/src/pools.ts
@@ -2,5 +2,5 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 // Pool IDs for known pools on Polkadot and Kusama networks
-export const PolkadotKnownPoolIds: number[] = [15, 19]
+export const PolkadotKnownPoolIds: number[] = [15, 19, 344]
 export const KusamaKnownPoolIds: number[] = [223]
