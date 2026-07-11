b237eb0b9a9501095e95a1aab2a8fa7bf253388e
Ross Bulat <ross@jkrb.io>
2026-07-11 21:15:28 +0700

    simplify component

--- packages/app-stablecoins/src/Router.tsx
+++ packages/app-stablecoins/src/Router.tsx
@@ -1,12 +1,14 @@
 // Copyright 2026 @polkadot-cloud/polkadot-cloud-apps authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import { StablecoinsDappName } from 'consts'
 import { Headers } from 'library/Headers'
 import { SideMenu } from 'library/SideMenu'
 import { ErrorBoundary } from 'react-error-boundary'
 import { HelmetProvider } from 'react-helmet-async'
-import { useTranslation } from 'react-i18next'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import type { PageItem } from 'types'
 import { ErrorFallbackApp, ErrorFallbackRoutes } from 'ui-app/ErrorBoundary'
 import { MainFooter } from 'ui-app/MainFooter'
 import { Offline } from 'ui-app/Offline'
@@ -15,16 +17,14 @@ import { Page } from 'ui-core/base'
 import { Overlays } from './Overlays'
 import { Send } from './pages/Send'
 
-const AppTitle = 'Polkadot Cloud Stablecoins'
-
-const SendPage = () => {
-	const { t } = useTranslation('app')
-
-	return (
-		<PageWithTitle title={t('send')} appTitle={AppTitle}>
-			<Send />
-		</PageWithTitle>
-	)
+const SendPage: PageItem = {
+	category: 0,
+	key: 'send',
+	uri: `${import.meta.env.BASE_URL}send`,
+	hash: '/send',
+	Entry: Send,
+	faIcon: faPaperPlane,
+	advanced: false,
 }
 
 export const Router = () => (
@@ -38,7 +38,15 @@ export const Router = () => (
 					<ErrorBoundary FallbackComponent={ErrorFallbackRoutes}>
 						<Routes>
 							<Route index element={<Navigate to="/send" replace />} />
-							<Route path="/send" element={<SendPage />} />
+							<Route
+								path="/send"
+								element={
+									<PageWithTitle
+										page={SendPage}
+										appTitle={StablecoinsDappName}
+									/>
+								}
+							/>
 							<Route path="*" element={<Navigate to="/send" replace />} />
 						</Routes>
 					</ErrorBoundary>
--- packages/ui-app/src/PageWithTitle/index.tsx
+++ packages/ui-app/src/PageWithTitle/index.tsx
@@ -1,43 +1,27 @@
 // Copyright 2026 @polkadot-cloud/polkadot-cloud-apps authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import type { ReactNode } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { useTranslation } from 'react-i18next'
 import type { PageItem } from 'types'
 import { Page } from 'ui-core/base'
 
-type PageWithTitleProps =
-	| {
-			appTitle?: string
-			page: PageItem
-			children?: never
-			title?: never
-	  }
-	| {
-			appTitle: string
-			children: ReactNode
-			page?: never
-			title: string
-	  }
+type PageWithTitleProps = {
+	appTitle?: string
+	page: PageItem
+}
 
 export const PageWithTitle = (props: PageWithTitleProps) => {
 	const { t } = useTranslation()
+	const { Entry, key } = props.page
 	const appTitle = props.appTitle ?? t('title', { ns: 'app' })
-	const pageTitle = props.page ? t(props.page.key, { ns: 'app' }) : props.title
-
-	const content = props.page ? (
-		<props.page.Entry page={props.page} />
-	) : (
-		props.children
-	)
 
 	return (
 		<Page.Container>
 			<Helmet>
-				<title>{`${pageTitle} | ${appTitle}`}</title>
+				<title>{`${t(key, { ns: 'app' })} | ${appTitle}`}</title>
 			</Helmet>
-			{content}
+			<Entry page={props.page} />
 		</Page.Container>
 	)
 }
