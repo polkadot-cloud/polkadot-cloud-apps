bb0174af3eadccb9f5adc7217b6e516c5f26d420
Ross Bulat <ross@jkrb.io>
2026-07-13 09:59:53 +0700

    feat: support custom transaction fee display

--- packages/ui-app/src/EstimatedTxFee/types.ts
+++ packages/ui-app/src/EstimatedTxFee/types.ts
@@ -1,6 +1,9 @@
 // Copyright 2026 @polkadot-cloud/polkadot-cloud-apps authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
+import type { TxFeeDisplay } from 'types'
+
 export interface EstimatedTxFeeProps {
 	uid?: number
+	feeDisplay: TxFeeDisplay
 }
