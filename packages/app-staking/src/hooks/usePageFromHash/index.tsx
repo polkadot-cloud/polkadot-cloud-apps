2d1a301ab618519dd6595cfacbff3eebb878a61b
Ross Bulat <ross@jkrb.io>
2026-07-13 19:23:59 +0700

    Revert "feat(refactor): Move `usePageFromHash` hook (#3655)"
    This reverts commit 1e6a6e6d8a688516c09a5cb5ae49fd7984636d92.

--- packages/app-staking/src/hooks/usePageFromHash/index.tsx
+++ packages/app-staking/src/hooks/usePageFromHash/index.tsx
@@ -1,23 +1,15 @@
 // Copyright 2026 @polkadot-cloud/polkadot-cloud-apps authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
+import { PageCategories, PagesConfig } from 'config'
 import { useLocation } from 'react-router-dom'
-import type { PageCategoryItems, PagesConfigItems } from 'types'
 import { getCategoryFromPage } from 'utils'
 
-interface UsePageFromHashProps {
-	pageCategories: PageCategoryItems
-	pagesConfig: PagesConfigItems
-}
-
-export const usePageFromHash = ({
-	pageCategories,
-	pagesConfig,
-}: UsePageFromHashProps) => {
+export const usePageFromHash = () => {
 	const { pathname } = useLocation()
 	const page = (pathname ?? '').replace(/^#?\/+/, '').split('?')[0]
 
-	const categoryKey = getCategoryFromPage(pageCategories, pagesConfig, page)
+	const categoryKey = getCategoryFromPage(PageCategories, PagesConfig, page)
 
 	return { page, categoryKey }
 }
