144d2040d4cef85e916a3a56c45e6032070d2ccb
Ross Bulat <ross@jkrb.io>
2026-07-20 12:36:21 +0700

    fix

--- packages/ui-app/src/SideMenu/index.tsx
+++ packages/ui-app/src/SideMenu/index.tsx
@@ -1,10 +1,7 @@
 // Copyright 2026 @polkadot-cloud/polkadot-cloud-apps authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-export {
-	CategoriesPopover,
-	type CategoriesPopoverProps,
-} from './Categories'
+export { CategoriesPopover } from './Categories'
 export { DefaultMenu } from './DefaultMenu'
 export { FloatingMenu } from './FloatingMenu'
 export { NavSimple } from './NavSimple'
