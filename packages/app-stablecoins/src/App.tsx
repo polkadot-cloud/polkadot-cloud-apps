ddb757723f12871c56be26cce70a3d8cf11c74b3
Ross Bulat <ross@jkrb.io>
2026-07-21 12:36:59 +0700

    stablecoins skeleton 1

---
+++ packages/app-stablecoins/src/App.tsx
@@ -0,0 +1,12 @@
+// Copyright 2026 @polkadot-cloud/polkadot-cloud-apps authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { i18next } from 'locales'
+import { I18nextProvider } from 'react-i18next'
+import { Providers } from './Providers'
+
+export const App = () => (
+	<I18nextProvider i18n={i18next}>
+		<Providers />
+	</I18nextProvider>
+)
---
+++ packages/app-stablecoins/src/Providers.tsx
@@ -0,0 +1,30 @@
+// Copyright 2026 @polkadot-cloud/polkadot-cloud-apps authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { ConnectProvider } from '@polkadot-cloud/connect'
+import { LedgerAdaptor } from '@polkadot-cloud/connect-ledger'
+import { StablecoinsDappName } from 'consts'
+import { Tooltip } from 'radix-ui'
+import { BrowserRouter } from 'react-router-dom'
+import { OverlayProvider } from 'ui-overlay'
+import { ThemedRouter } from './Themes'
+
+const Network = 'polkadot'
+const PolkadotSs58 = 0
+
+export const Providers = () => (
+	<ConnectProvider
+		network={Network}
+		dappName={StablecoinsDappName}
+		ss58={PolkadotSs58}
+		adaptors={[LedgerAdaptor]}
+	>
+		<BrowserRouter>
+			<OverlayProvider>
+				<Tooltip.Provider>
+					<ThemedRouter />
+				</Tooltip.Provider>
+			</OverlayProvider>
+		</BrowserRouter>
+	</ConnectProvider>
+)
