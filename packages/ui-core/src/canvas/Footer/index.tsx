be25f114fd98fd9c96ea90624fdc29d98292bb7c
Ross Bulat <ross@jkrb.io>
2026-08-18 11:13:01 +0700

    add light footer container

--- packages/ui-core/src/canvas/Footer/index.tsx
+++ packages/ui-core/src/canvas/Footer/index.tsx
@@ -2,20 +2,25 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 import classNames from 'classnames'
-import type { CanvasSize, ComponentBase } from 'types'
+import type { CanvasSize, ComponentBaseWithClassName } from 'types'
 import classes from './index.module.scss'
 
 export const Footer = ({
 	children,
+	className,
 	style,
 	size,
-}: ComponentBase & {
+}: ComponentBaseWithClassName & {
 	size?: CanvasSize
 }) => {
-	const allClasses = classNames(classes.footer, {
-		[classes.lg]: size === 'lg',
-		[classes.xl]: size === 'xl',
-	})
+	const allClasses = classNames(
+		classes.footer,
+		{
+			[classes.lg]: size === 'lg',
+			[classes.xl]: size === 'xl',
+		},
+		className,
+	)
 
 	return (
 		<div className={allClasses} style={style}>
