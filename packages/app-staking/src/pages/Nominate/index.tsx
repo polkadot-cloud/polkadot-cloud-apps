c18e7a7054e3388d42060982efb22666807fb51a
Ross Bulat <ross@jkrb.io>
2026-09-11 12:36:53 +0700

    chore: remove polkawatch plugin

--- packages/app-staking/src/pages/Nominate/index.tsx
+++ packages/app-staking/src/pages/Nominate/index.tsx
@@ -1,61 +1,20 @@
 // Copyright 2026 @polkadot-cloud/polkadot-cloud-apps authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
-import { onTabVisitEvent } from 'event-tracking'
-import { usePlugins } from 'hooks/usePlugins'
 import { NominationWarnings } from 'library/NominationWarnings'
-import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { PageTabs } from 'ui-app/PageTabs'
 import { Page } from 'ui-core/base'
 import { Active } from './Active'
-import { NominationGeo } from './NominationGeo'
 import { Wrapper } from './Wrappers'
 
 export const Nominate = () => {
 	const { t } = useTranslation()
-	const { pluginEnabled } = usePlugins()
-
-	const [activeTab, setActiveTab] = useState<number>(0)
-
-	// Go back to the first tab if the Polkawatch plugin is disabled
-	useEffect(() => {
-		if (!pluginEnabled('polkawatch')) {
-			setActiveTab(0)
-		}
-	}, [pluginEnabled('polkawatch')])
 
 	return (
 		<Wrapper>
-			<Page.Title title={t('nominate', { ns: 'pages' })}>
-				<PageTabs
-					tabs={
-						pluginEnabled('polkawatch')
-							? [
-									{
-										title: t('overview', { ns: 'app' }),
-										active: activeTab === 0,
-										onClick: () => {
-											onTabVisitEvent('nominate', 'overview')
-											setActiveTab(0)
-										},
-									},
-									{
-										title: t('decentralization', { ns: 'app' }),
-										active: activeTab === 1,
-										onClick: () => {
-											onTabVisitEvent('nominate', 'decentralization')
-											setActiveTab(1)
-										},
-									},
-								]
-							: undefined
-					}
-				/>
-			</Page.Title>
+			<Page.Title title={t('nominate', { ns: 'pages' })} />
 			<NominationWarnings />
-			{activeTab === 0 && <Active />}
-			{activeTab === 1 && <NominationGeo />}
+			<Active />
 		</Wrapper>
 	)
 }
